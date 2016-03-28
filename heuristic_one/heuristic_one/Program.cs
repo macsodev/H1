b0b2813d56e29d83b7b4f83f0d761cd7a4b42c1a
macsodev <macsodev@gmail.com>
2016-03-28 17:09:14 +0200

    Add algorithm and swap setpixel with set cursor pos+mark

--- heuristic_one/heuristic_one/Program.cs
+++ heuristic_one/heuristic_one/Program.cs
@@ -10,8 +10,45 @@ namespace heuristic_one
     {
         static void Main(string[] args)
         {
-            //kezdjunk neki :)
+            //kezdjunk neki :) 
+            
 
+        }
+        public void line(int x, int y, int x2, int y2)
+        {
+            int w = x2 - x;
+            int h = y2 - y;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write("x");
+
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else {
+                    x += dx2;
+                    y += dy2;
+                }
+            }
         }
     }
 }
