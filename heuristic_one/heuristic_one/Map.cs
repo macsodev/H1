9767ef43f166a7d0a23c4f426e90a389e423c07c
Roland <Roland@ROLAND-PC>
2016-04-03 18:12:04 +0200

    Bresenham2 method added to class Map

--- heuristic_one/heuristic_one/Map.cs
+++ heuristic_one/heuristic_one/Map.cs
@@ -109,6 +109,66 @@ namespace heuristic_one
 
         }
         
+		// Draw a line from Start point in Direction (degrees) until obstacle
+        public Coords Bresenham2(Coords Start, double Direction)
+        {
+            List<Coords> L = new List<Coords>();
+            int x = Start.GetX();
+            int y = Start.GetY();
+            int dx, dy;
+            double St, Steepness;
+            
+			dy = 0; // Horizontal line
+			dx = 0; // Vertical line
+
+            // Right or left
+            if (Direction < 90 || Direction > 270) dx = 1;
+            else if (Direction > 90 && Direction < 270) dx = -1;
+
+            // Up or down
+            if (Direction < 180) dy = -1;
+            else if (Direction > 180) dy = 1;
+            
+			// 90 or 270 degrees are invalid on a tangent
+			if(Direction == 90 || Direction == 270) {
+				Steepness = 1;
+			} else {
+	            Steepness = Math.Abs(Math.Tan(Direction / 180.0 * Math.PI));
+			}
+			
+            if (Steepness <= 1) {
+                St = y + (dy * Steepness);
+                x += dx;
+                y += (int)Math.Round(dy * Steepness);
+            } else {
+                St = x + (dx / Steepness);
+                x += (int)Math.Round(dx / Steepness);
+                y += dy;
+            }
+
+            // Drawing
+            while (x >= 0 && y >= 0 && x < GetWidth() && y < GetHeight()
+                && Matrix[y, x] == Empty)
+            {
+                Matrix[y, x] = 'x';
+                L.Add(new Coords(x,y));
+
+                if (Steepness <= 1) {
+                    x += dx;
+                    St += dy * Steepness;
+                    y = (int)Math.Round(St);
+                } else {
+                    St += dx / Steepness;
+                    x = (int)Math.Round(St);
+                    y += dy;
+                }
+            }
+
+            if (L.Count > 0) return L.ElementAt(L.Count - 1);
+            else return Start;
+        }
+
+
         public override String ToString()
         {
             StringBuilder s = new StringBuilder();
